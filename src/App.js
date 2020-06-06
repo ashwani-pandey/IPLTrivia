39bbdccb6180a13166abb76160c68235edc19652
ashwani-pandey <thisisashwanipandey@gmail.com>
2020-06-06 21:05:04 +0530

    complete the basic overall design

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TeamsView from "./components/TeamsView";
 function App() {
   return (
     <div className="App">
+      <div className="head-text">
+        Enjoy some amazing IPL Trivia.<br /> Any guesses what's behind each card?!
+      </div>
       <TeamsView />
     </div>
   );
